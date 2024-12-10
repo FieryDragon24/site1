5df777a0fd70aed306b0d85ce83956ca826c71ff
FieryDragon24 <164327458+FieryDragon24@users.noreply.github.com>
2024-12-11 01:15:34 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,61 @@
+const galleryContainer = document.querySelector(".gallery-container");
+const galleryControlsContainer = document.querySelector(".gallery-controls");
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
+
+class Carousel {
+    constructor(container, items, controls) {
+        this.carouselContainer = container;
+        this.carouselControls = controls;
+        this.carouselArray = [...items];
+    }
+
+    updateGallery() {
+        this.carouselArray.forEach(el => {
+            el.classList.remove('gallery-item-1');
+            el.classList.remove('gallery-item-2');
+            el.classList.remove('gallery-item-3');
+            el.classList.remove('gallery-item-4');
+            el.classList.remove('gallery-item-5');
+        });
+
+        this.carouselArray.slice(0, 5).forEach((el, i) => {
+            el.classList.add(`gallery-item-${i + 1}`);
+        });
+    }
+
+    setCurrentState(control) {
+        if (control.classList.contains('gallery-controls-previous')) {
+            // Previous button: Move the last item to the front
+            this.carouselArray.unshift(this.carouselArray.pop());
+        } else if (control.classList.contains('gallery-controls-next')) {
+            // Next button: Move the first item to the end
+            this.carouselArray.push(this.carouselArray.shift());
+        }
+        this.updateGallery();
+    }
+
+    setControls() {
+        this.carouselControls.forEach(control => {
+            const button = document.createElement('button');
+            button.className = `gallery-controls-${control}`;
+            button.innerText = control;
+            galleryControlsContainer.appendChild(button);
+        });
+    }
+
+    useControls() {
+        const triggers = [...galleryControlsContainer.childNodes];
+        triggers.forEach(control => {
+            control.addEventListener('click', e => {
+                e.preventDefault();
+                this.setCurrentState(control);
+            });
+        });
+    }
+}
+
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exampleCarousel.setControls();
+exampleCarousel.useControls();
